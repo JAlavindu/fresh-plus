02ff6b369c12b59e63d17a4a88f69e36f0709b9e
lavindu perera <lavi156perera@gmail.com>
2024-06-29 22:14:19 +0530

    update

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -22,7 +22,6 @@ const Home = ({ setShowLogin, setUserName }) => {
 
       {!token ? (
         <div>
-          <FoodDisplay selectedAdmin={""} clickedAll={true} />
           <Details />
           <HowItWorks />
           <AboutUs />
