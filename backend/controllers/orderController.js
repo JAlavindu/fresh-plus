f4b89dbf8c844e3b3bfadc56a431bc545e5455f4
gamithf <gamithfernando311@gmail.com>
2024-06-30 08:29:18 +0530

    Implement send confirmation mail

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,15 +1,23 @@
 import orderModel from "../models/orderModel.js";
-import userModel from "../models/userModel.js"
-import Stripe from "stripe"
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
 import mongoose from "mongoose";
+import nodemailer from "nodemailer";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// Configure nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: "freshstoreofficial2002@gmail.com",
+        pass: process.env.GMAIL_APP_PASSWORD // Use the App Password here
+    }
+});
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-
-    const frontend_url = "http://localhost:5173"
-
+    const frontend_url = "http://localhost:5173";
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -17,10 +25,10 @@ const placeOrder = async (req, res) => {
             items: req.body.items,
             amount: req.body.amount,
             address: req.body.address,
-        })
+        });
 
         await newOrder.save();
-        await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}});
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData: {}});
 
         const line_items = req.body.items.map((item) => ({
             price_data: {
@@ -31,7 +39,7 @@ const placeOrder = async (req, res) => {
                 unit_amount: item.price
             },
             quantity: item.quantity
-        }))
+        }));
 
         line_items.push({
             price_data: {
@@ -42,131 +50,128 @@ const placeOrder = async (req, res) => {
                 unit_amount: 200
             },
             quantity: 1
-        })
+        });
 
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: "payment",
             success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+        });
 
-        })
-
-        res.json({success: true, session_url: session.url})
-
-
+        res.json({ success: true, session_url: session.url });
     } catch (error) {
         console.log(error);
-        res.json({success: false, message: "Error"})
+        res.json({ success: false, message: "Error" });
     }
-
-}
+};
 
 const verifyOrder = async (req, res) => {
-    const {orderId, success} = req.body;
-
+    const { orderId, success } = req.body;
     try {
-        if(success=="true") {
-            await orderModel.findByIdAndUpdate(orderId, {payment:true});
-            res.json({success: true, message: "Paid"})
-        }
-        else {
+        if (success === "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" });
+        } else {
             await orderModel.findByIdAndDelete(orderId);
-            res.json({success: false, message: "Not Paid"})
+            res.json({ success: false, message: "Not Paid" });
         }
     } catch (error) {
         console.log(error);
-        res.json({success: false, message: "Error"})
-        
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
 // get user orders
 const userOrders = async (req, res) => {
     try {
-        const orders = await orderModel.find({userId: req.body.userId})
-        res.json({success:true, data:orders})
-        
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data: orders });
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error"})
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
 // Listing orders for admin
 const listOrders = async (req, res) => {
     try {
         const orders = await orderModel.find({});
-        res.json({success: true, data: orders})
+        res.json({ success: true, data: orders });
     } catch (error) {
         console.log(error);
-        res.json({sucess: false, message: "Error"})   
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
 // api for updating order status
 const updateStatus = async (req, res) => {
     try {
-        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status})
-        res.json({success: true, message: "Status Updated"})
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: "Status Updated" });
     } catch (error) {
         console.log(error);
-        res.json({success: false, message: "Error"})
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
 // get admin orders
 const adminOrders = async (req, res) => {
     try {
-        const {userId} = req.body;
-
+        const { userId } = req.body;
         if (!userId) {
             return res.status(400).json({ success: false, message: "userId is required" });
         }
-
         if (!mongoose.isValidObjectId(userId)) {
             return res.status(400).json({ success: false, message: "Invalid userId format" });
         }
-
-        const orders = await orderModel.find({ adminId: userId })
-        // console.log(req.body);
-        res.json({success:true, data:orders})
-        
+        const orders = await orderModel.find({ adminId: userId });
+        res.json({ success: true, data: orders });
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error"})
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
 const adminConfirmOrder = async (req, res) => {
     try {
-        const {orderId} = req.body;
-        
+        const { orderId } = req.body;
         if (!orderId) {
             return res.status(400).json({ success: false, message: "orderId is required" });
         }
-        
         if (!mongoose.isValidObjectId(orderId)) {
             return res.status(400).json({ success: false, message: "Invalid userId format" });
         }
-        
-        const order = await orderModel.findOne({ _id: orderId });
 
+        const order = await orderModel.findOne({ _id: orderId });
         if (!order) {
             return res.status(400).json({ success: false, message: "order not found" });
         }
 
-        // console.log(order.status);
         order.status = "On Delivery";
         await order.save();
 
-        res.json({success:true, data: order});
-        
+        const user = await userModel.findOne({ _id: order.userId });
+
+        const mailOptions = {
+            from: "freshstoreofficial2002@gmail.com",
+            to: order.address.email,
+            subject: 'Your Order is On Delivery',
+            text: `Hello ${user.name},\n\nYour order with ID ${orderId} is now on delivery.\n\nThank you for shopping with us!`
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ success: false, message: "Failed to send email" });
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        res.json({ success: true, data: order });
     } catch (error) {
         console.log(error);
-        res.json({success:false, message:"Error"})
+        res.json({ success: false, message: "Error" });
     }
-}
+};
 
-
-export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus, adminOrders, adminConfirmOrder}
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus, adminOrders, adminConfirmOrder };
